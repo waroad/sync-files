7c706ea996207210dc93c0234c7d08e5017fee01
waroad <78641034+waroad@users.noreply.github.com>
2022-11-15 22:35:19 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -48,7 +48,7 @@ main() {
     checkOrAddSyncIgnore
     git diff source/${SYNC_BRANCH_NAME} -R | git apply --reject --whitespace=fix
     git add $(git ls-tree --name-only -r source/${SYNC_BRANCH_NAME} | grep -E "${REGEX}")
-    git diff --name-only > foo.txt
+    git diff source/${SYNC_BRANCH_NAME} -R | git apply --reject --whitespace=fix --name-only > foo.txt
     git add foo.txt
     git restore -- ${SYNC_IGNORE_FILE_NAME}
     for deletedFile in $deletedFiles; do
