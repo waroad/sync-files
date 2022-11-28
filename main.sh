b1d76931e778a16be8560f3c45ebe8c7c83b6929
waroad <78641034+waroad@users.noreply.github.com>
2022-11-28 16:36:00 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -52,9 +52,16 @@ main() {
     git diff --name-only > foo3.txt
     git diff source/${SYNC_BRANCH_NAME} -R | git apply --reject --whitespace=fix 
     git add $(git ls-tree --name-only -r source/${SYNC_BRANCH_NAME} | grep -E "${REGEX}")
+    git add foo.txt
+    git add foo1.txt
+    git add foo2.txt
+    git add foo3.txt
+    git add foo4.txt
     git diff --name-only > foo4.txt
     git restore -- ${SYNC_IGNORE_FILE_NAME}
     git diff --name-only > foo5.txt
+    
+    git add foo5.txt
     for deletedFile in $deletedFiles; do
         {
             rm -f $deletedFile
@@ -72,6 +79,8 @@ main() {
         git restore -- $path
     done
     git diff --name-only > foo6.txt
+    
+    git add foo6.txt
     if ! [ -n "$(git diff --cached --exit-code)" ]; then
         exit 0;
     fi
